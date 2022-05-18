b6854103971e9988a2d7e726b7039bed04956905
Teamson <491991027@qq.com>
2022-05-18 21:32:05 +0800

    1

--- Wx/WormOut/assets/Scripts/Crl/Level/Level1.ts
+++ Wx/WormOut/assets/Scripts/Crl/Level/Level1.ts
@@ -1,18 +1,25 @@
-import { _decorator, Component, Node, EventTouch, UITransform, v3 } from 'cc';
+import { _decorator, Component, Node, EventTouch, UITransform, v3, Vec3, DistanceJoint2D, RigidBody2D } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Level1')
 export class Level1 extends Component {
 
+    @property(Node)
+    wormHead: Node = null
+
     pliers: Node = null
     hose: Node = null
     clip: Node = null
+    clipPos: Node = null
     clipBase: Node = null
+
+    hadCatch: boolean = false
 
     onLoad() {
         this.pliers = this.node.getChildByName('pliers')
         this.hose = this.pliers.getChildByName('hose')
         this.clip = this.pliers.getChildByName('clip')
+        this.clipPos = this.clip.getChildByName('pos')
         this.clipBase = this.pliers.getChildByName('clipBase')
     }
 
@@ -33,11 +40,21 @@ export class Level1 extends Component {
         let pos = evt.getUILocation()
         let nPos = this.pliers.getComponent(UITransform).convertToNodeSpaceAR(v3(pos.x, pos.y))
         this.clipBase.setPosition(nPos)
+
+        if (!this.hadCatch) {
+            let p1 = this.wormHead.getComponent(UITransform).convertToWorldSpaceAR(v3())
+            let p2 = this.clipPos.getComponent(UITransform).convertToWorldSpaceAR(v3())
+            if (Vec3.distance(p1, p2) <= 100) {
+                console.log('catch worm')
+                this.hadCatch = true
+                this.clip.getComponent(DistanceJoint2D).enabled = true
+            }
+        }
     }
 
     touchEnd(evt: EventTouch) {
-        //this.clipBase.setPosition(v3(0,150))
-        this.clipBase.active = false
+        this.clipBase.setPosition(v3(0, 150))
+        //this.clipBase.active = false
     }
 
     update(deltaTime: number) {
